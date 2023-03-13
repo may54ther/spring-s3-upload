f5cf1cdb8ff7b24179cf7b80c70ff4321ea3d0b7
may54ther <aha9654@gmail.com>
2023-03-13 18:07:35 +0900

    feat: jasypt 설정

---
+++ src/main/java/io/ahakim/file/config/JasyptConfig.java
@@ -0,0 +1,27 @@
+package io.ahakim.file.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor(@Value("${jasypt.encryptor.password}") String secreteKey) {
+
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(secreteKey);
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
